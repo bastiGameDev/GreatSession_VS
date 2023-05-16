088109cf7fcdc130495f41e6f86449f3f1785403
MishaZaviyalov <97869420+MishaZaviyalov@users.noreply.github.com>
2023-05-16 10:39:03 +0300

    init3

---
+++ Testing/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Testing")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Testing")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("822998ad-575a-442d-871c-d799fc59d5f0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Testing/ValidationTests.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Text.RegularExpressions;
+using SESSIONWPF;
+
+namespace Testing
+{
+    [TestClass]
+    public class ValidationTests
+    {
+        // Пароль не должен состоять из логина!
+        [TestMethod]
+        public void ifPassSameAsLogin_enterString_falseReterned()
+        {
+            ValidationChecker checker = new ValidationChecker();
+            string login = "loginDEbho2018";
+            string password = "loginDEbho2018";
+
+            bool isSame = checker.PasswordStringValidation(login, password);
+            Assert.IsFalse(isSame);
+        }
+
+        // Пароль должен содержать от 5 до 20 символов!
+        [TestMethod]
+        public void ifPassLengthIsBetween4and20_enterString_falseReterned()
+        {
+            ValidationChecker checker = new ValidationChecker();
+            string login = "Mom";
+            string password = "Mom";
+
+            bool isShort = checker.PasswordStringValidation(login, password);
+            Assert.IsFalse(isShort);
+        }
+
+        // Пароль должен содержать буквы верхнего регистра!
+        [TestMethod]
+        public void ifPassContainsUpperCaseLetters_enterString_falseReterned()
+        {
+            ValidationChecker checker = new ValidationChecker();
+            string login = "password";
+            string password = "password";
+
+            bool isUpper = checker.PasswordStringValidation(login, password);
+            Assert.IsFalse(isUpper);
+        }
+
+        // Пароль должен содержать буквы нижнего регистра!
+        [TestMethod]
+        public void ifPassContainsLowerCaseLetters_enterString_falseReterned()
+        {
+            ValidationChecker checker = new ValidationChecker();
+            string login = "PASSWORD";
+            string password = "PASSWORD";
+
+            bool isLower = checker.PasswordStringValidation(login, password);
+            Assert.IsFalse(isLower);
+        }
+    }
+}
